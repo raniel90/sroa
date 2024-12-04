6e8d00d7592b426c82c37e19deae60545e37be0a
Raniel Silva <raniel90@gmail.com>
2024-12-03 22:15:52 -0300

    fix port config

--- src/app.local.ts
+++ src/app.local.ts
@@ -4,8 +4,8 @@ import MongoService from './lib/mongodb';
 
 // Call to app.listen to create local server for dev env.
 MongoService.getConnection().then((connection) => {
-  const port = parseInt(process.env['port'], 10) || 3300;
-  app.listen(port, '0.0.0.0');
+  const port = process.env.PORT || 8080;
+  app.listen(port);
 
   console.log('App listenting on :', port);
 });
