2b2f3192e38fbc7a4cce88b0f68f00bf489c68fd
Raniel Silva <raniel90@gmail.com>
2024-12-03 22:34:39 -0300

    fix connection mongoDB

--- src/app.local.ts
+++ src/app.local.ts
@@ -3,9 +3,15 @@ import app from './app';
 import MongoService from './lib/mongodb';
 
 // Call to app.listen to create local server for dev env.
-MongoService.getConnection().then((connection) => {
-  const port = process.env.PORT || 8080;
-  app.listen(port);
+MongoService.getConnection()
+  .then((connection) => {
+    const port = process.env.PORT || 8080;
+    app.listen(port, () => {
+      console.log('App listening on:', port);
+    });
+  })
+  .catch((error) => {
+    console.error('Failed to connect to MongoDB:', error);
+    process.exit(1); // Exit the process with an error code
+  });
 
-  console.log('App listenting on :', port);
-});
